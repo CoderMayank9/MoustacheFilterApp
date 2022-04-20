b6ee791f4e8cf841bcdbbea2f703e8bfa19f8755
CoderMayank9 <84262175+CoderMayank9@users.noreply.github.com>
2022-04-20 18:24:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,10 +6,26 @@ function setup()
 {
 Canvas=createCanvas(400,400);
 Canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+posenet=ml5.poseNet(video,modelloaded);
+posenet.on('pose',gotposes);
+}
+function modelloaded()
+{
+console.log("posenet is initialized");
+}
+function gotposes(results)
+{
+if(results.length>0)
+{
+console.log(results);
+}
 }
 function draw()
 {
-
+image(video,0,0,400,400);
 }
 function snapshot()
 {
